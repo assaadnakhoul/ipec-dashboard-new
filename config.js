e4dc0db09dacd9c8d82dfe13b3f02c16f0c8f31a
assaadnakhoul <assaad.nakhoul@gmail.com>
2025-08-17 21:50:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
-<script>
-/***********************
- * config.js
- ***********************/
-
-// ✅ MUST be plural (array of URLs)
 window.JSON_URLS = [
   "https://script.google.com/macros/s/AKfycbwsvBV8yNbAzvaapA4H_WkFPC6PmALh4f8ohg8mXmx81CJ_81sAS5uJGQ3uQ7CHgLr1Iw/exec"
 ];
 
-// Images (optional)
 window.IMAGES_BASE = "./public/images/";
 window.IMAGE_EXTS  = [".webp", ".jpg", ".png"];
-</script>
