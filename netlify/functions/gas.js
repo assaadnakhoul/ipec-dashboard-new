c43e9e309a29fd02620f45fe468a85fd8aaa5551
assaadnakhoul <assaad.nakhoul@gmail.com>
2025-08-24 00:45:23 +0200

    Update gas.js

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -3,14 +3,14 @@ exports.handler = async () => {
   try {
     const GAS_URL = "https://script.google.com/macros/s/AKfycbywz_pa45tfEIgDH3rjHFr_pcXfGtgfAXjj960lVTmovF_u96B5p8a7XLqhHIjwdK70WA/exec";
     const r = await fetch(GAS_URL, { redirect: "follow" });
-    const body = await r.text(); // GAS returns JSON text
+    const text = await r.text(); // GAS returns JSON text
     return {
       statusCode: r.ok ? 200 : r.status,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
       },
-      body
+      body: text
     };
   } catch (e) {
     return { statusCode: 500, body: JSON.stringify({ error: String(e) }) };
