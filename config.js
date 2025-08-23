6c0b2902e1ab25ef752776292cb56d5137e3f430
assaadnakhoul <assaad.nakhoul@gmail.com>
2025-08-23 12:03:13 +0200

    Changed the master sheet to assaadcharbel98 account
    Changed the master sheet to assaadcharbel98 account -> folder: https://drive.google.com/drive/folders/1VGu3PTznsnFDkuzZFlNrBhxgmwc60UOT

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.JSON_URLS = [
+  "https://script.google.com/macros/s/AKfycbwJzsMoze5bFWutKp7VZ-Tgo5kcvEr2aU_R9gvunDpcdl71Vpq4BRFXclYmSxuz292pYw/exec",
   "https://script.google.com/macros/s/AKfycbwsvBV8yNbAzvaapA4H_WkFPC6PmALh4f8ohg8mXmx81CJ_81sAS5uJGQ3uQ7CHgLr1Iw/exec"
 ];
 
