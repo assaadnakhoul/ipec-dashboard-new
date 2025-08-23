402e5262722d0b6d6e697c748dd1113624c2c5b6
assaadnakhoul <assaad.nakhoul@gmail.com>
2025-08-24 00:57:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js (dashboard)
 window.JSON_URLS = [
-  "https://script.google.com/macros/s/AKfycbywz_pa45tfEIgDH3rjHFr_pcXfGtgfAXjj960lVTmovF_u96B5p8a7XLqhHIjwdK70WA/exec?json=1&id=1YGexar8HPS46hMDGXeajmfFfk9-sbuYUdEP-04HvRj4&gid=PUT_YOUR_GID_HERE"
+  "https://script.google.com/macros/s/AKfycbywz_pa45tfEIgDH3rjHFr_pcXfGtgfAXjj960lVTmovF_u96B5p8a7XLqhHIjwdK70WA/exec?json=1&id=1YGexar8HPS46hMDGXeajmfFfk9-sbuYUdEP-04HvRj4&gid=0"
 ];
 
 window.IMAGES_BASE = "./public/images/";
