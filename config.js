7f44655364a7984a997214b133273090842ce297
assaadnakhoul <assaad.nakhoul@gmail.com>
2025-08-24 15:57:59 +0200

    Assaad

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+// config.js
+// Primary JSON source (add more as fallbacks if you want)
 window.JSON_URLS = [
   "https://script.google.com/macros/s/AKfycbywz_pa45tfEIgDH3rjHFr_pcXfGtgfAXjj960lVTmovF_u96B5p8a7XLqhHIjwdK70WA/exec?json=1&id=1YGexar8HPS46hMDGXeajmfFfk9-sbuYUdEP-04HvRj4&gid=0"
 ];
 
+// Optional image settings (safe defaults)
 window.IMAGES_BASE = "./public/images/";
-window.IMAGE_EXTS  = [".webp", ".jpg", ".png"];
+window.IMAGE_EXTS = [".webp", ".jpg", ".png"];
